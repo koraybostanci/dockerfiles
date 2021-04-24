49ec8823ba9961d5f5e5ea6f1db6f467bf903cc0
Koray Bostanci <bostanci.koray@gmail.com>
2021-04-24 20:10:12 +0200

    run apps in the dev network

--- run-compose.sh
+++
@@ -1,7 +0,0 @@
-# Data
-docker-compose -f data/compose.yaml up -d --build
-docker-compose -f data/compose.yaml down
-
-# Monitoring
-docker-compose -f monitoring/compose.yaml up -d --build
-docker-compose -f monitoring/compose.yaml down
