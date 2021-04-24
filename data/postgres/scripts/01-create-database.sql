ad76763781eefdd637a8662dec0d0953bff44b6f
Koray Bostancı <koraybostanci@users.noreply.github.com>
2021-04-24 20:14:31 +0200

    run apps in the dev network (#10)

--- data/postgres/scripts/01-create-database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE core WITH 
-    OWNER postgres
-    TEMPLATE template1;
-
-GRANT ALL PRIVILEGES ON DATABASE core TO postgres;
-
-CREATE SCHEMA app;
-CREATE SCHEMA config;
-CREATE SCHEMA temp;
